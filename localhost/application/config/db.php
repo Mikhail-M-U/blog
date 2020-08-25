17d00c9d4e6644d265e8cd2382be7760edfdc47c
Mikhail-M-U <69292525+Mikhail-M-U@users.noreply.github.com>
2020-08-25 22:00:45 +0300

    Add files via upload
    Blog

---
+++ localhost/application/config/db.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'host' => 'localhost:3307',
+	'name' => 'blog',
+	'user' => 'root',
+	'password' => 'root',
+];
---
+++ localhost/application/views/main/about.php
@@ -0,0 +1,19 @@
+<header class="masthead" style="background-image: url('/public/images/about-bg.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="page-heading">
+                    <h1>Я начинающий програмист</h1>
+                    <span class="subheading">изучаю язык програмирования PHP</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <p>текст текст текст текст текст</p>
+        </div>
+    </div>
+</div>
